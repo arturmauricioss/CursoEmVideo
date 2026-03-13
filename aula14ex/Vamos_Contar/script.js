de2fcbbe98f4c1f4903259aaea0dd41ce9b87128
Artur Mauricio <arturmauricioss@outlook.com>
2026-03-12 22:22:21 -0300

    testando o loop para criar novos elementos

--- aula14ex/Vamos_Contar/script.js
+++ aula14ex/Vamos_Contar/script.js
@@ -1,54 +1,17 @@
-function carregar(){
-    var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('imagem')
-    var titulo = window.document.getElementById('titulo')
-    var rodape = window.document.getElementById('rodape')
+let res = document.querySelector("div#res")
+function contar(){
+    let inicio = document.querySelector("#inicio")
+    let fim = document.querySelector("#fim")
 
-    var data = new Date()
-    var hora = data.getHours()
-    var minutos = data.getMinutes()
-    msg.innerHTML=`${hora}:${minutos}`
+    let span = document.createElement('span')
+    span.innerHTML = `Hello`
+    res.appendChild(span)
+    
+    
 
-
-function texto_claro(){
-    titulo.style.color = 'white'
-    rodape.style.color = 'white'
-}
-function texto_escuro(){
-    titulo.style.color = 'black'
-    rodape.style.color = 'black'
-}
-
-    if (hora<5){
-        img.src = './assets/madrugada.png'
-        document.body.style.background = '#1A1A1A' 
-        texto_claro()
-    }
-    else if(hora<7){
-        img.src = './assets/amanhecer.png'
-        document.body.style.background = '#f9b394'
-        texto_escuro()
+    for (let index = inicio.value; index < fim.value; index++) {
+        const element = array[index];
         
     }
-    else if(hora<12){
-        img.src = './assets/dia.png'
-        document.body.style.background = '#4EA2EA'
-        texto_claro()
-    }
-    else if(hora<17){
-        img.src = './assets/tarde.png'
-        document.body.style.background = '#961b40'
-        texto_claro()
-    }
-    else if(hora<19){
-        img.src = './assets/entardecer.png'
-        document.body.style.background = '#FCE494' 
-        texto_escuro()
-        
-    }
-    else{
-        img.src = './assets/noite.png'
-        document.body.style.background = '#12121e'
-        texto_claro()
-    }
+
 }
