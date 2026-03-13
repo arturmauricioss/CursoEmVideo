da6aebbf8bcb3cd627ccc4aa6da3b69f8f484694
Artur Mauricio <arturmauricioss@outlook.com>
2026-03-12 22:55:43 -0300

    projeto vamos contar funcionando

--- aula14ex/Vamos_Contar/script.js
+++ aula14ex/Vamos_Contar/script.js
@@ -1,17 +1,26 @@
 let res = document.querySelector("div#res")
 function contar(){
     let inicio = document.querySelector("#inicio")
+    let ini = Number(inicio.value)
     let fim = document.querySelector("#fim")
+    let fini = Number(fim.value)
+    let passo = document.querySelector("#passo")
+    let passi = Number(passo.value)
 
-    let span = document.createElement('span')
-    span.innerHTML = `Hello`
-    res.appendChild(span)
-    
-    
 
-    for (let index = inicio.value; index < fim.value; index++) {
-        const element = array[index];
+    for (let index = ini; index <= fini; index += passi) {
+        let span = document.createElement('span')
+        span.innerHTML = `&nbsp ${index}`
         
+        let sum = index+passi
+        if(sum<=fini){
+            span.innerHTML += "->"
+        }
+        
+        
+        res.appendChild(span)
+
+
     }
 
 }
