caa66c296c707c49958f77cc77459ffc233d1b5b
Artur Maurício <arturmauricioss@outlook.com>
2026-03-20 15:17:12 -0300

    refatorando

--- aula14ex/Vamos_Contar/script.js
+++ aula14ex/Vamos_Contar/script.js
@@ -1,51 +1,43 @@
 let res = document.querySelector("div#res")
-function contar(){
-    let inicio = document.querySelector("#inicio")
-    let ini = Number(inicio.value)
-    let fim = document.querySelector("#fim")
-    let fini = Number(fim.value)
-    let passo = document.querySelector("#passo")
-    let passi = Number(passo.value)
+let inicio = document.querySelector("#inicio")
+let fim = document.querySelector("#fim")
+let passo = document.querySelector("#passo")
 
-    if (inicio.value===""){
-        inicio.value=0
-    }
-    if (fim.value===""){
-        fim.value=0
-    }
-    if (passo.value === "") {
-        passo.value = 1
-    }
+
+
+function contar(){
+    inicio.value = inicio.value || 0
+    fim.value = fim.value || 0
+    passo.value = passo.value || 1
+    let ini = Number(inicio.value)
+    let fini = Number(fim.value)
+    let passi = Number(passo.value)
 
     res.innerHTML =""
 
-    if(ini==fini){
-        let span = document.createElement('span')
-        span.innerHTML = `&nbsp ${ini}`
-        res.appendChild(span)
-    }
-    else if (ini<fini){
-    for (let index = ini; index <= fini; index += passi) {
+    function spamer(index,resu){
         let span = document.createElement('span')
         span.innerHTML = `&nbsp ${index}`
-        
-        let sum = index+passi
-        if(sum<=fini){
-            span.innerHTML += " ->"
+        if (resu===true){
+            span.innerHTML += "->"
         }
         res.appendChild(span)
+    }
+
+    if(ini===fini){
+        spamer(ini)
+    }
+
+    else if (ini<fini){
+    for (let index = ini; index <= fini; index += passi) {
+        let resu = index<fini-(passi/2)
+        spamer(index,resu)
     }
     }
     else if (fini<ini){
-    for (let index = ini; index >= fini; index -= passi) {
-        let span = document.createElement('span')
-        span.innerHTML = `&nbsp ${index}`
-        
-        let sum = index-passi
-        if(sum>=fini){
-            span.innerHTML += " ->"
+        for (let index = ini; index >= fini; index -= passi) {
+            let resu = index>fini+(passi/2)
+            spamer(index,resu)
         }
-        res.appendChild(span)
-    }
     }
 }
