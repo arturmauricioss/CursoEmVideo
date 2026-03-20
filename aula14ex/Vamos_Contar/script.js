c8777b423a624ca300c20382177157c034d3c521
Artur Maurício <arturmauricioss@outlook.com>
2026-03-20 15:49:49 -0300

    limpando o codigo

--- aula14ex/Vamos_Contar/script.js
+++ aula14ex/Vamos_Contar/script.js
@@ -3,24 +3,29 @@ let inicio = document.querySelector("#inicio")
 let fim = document.querySelector("#fim")
 let passo = document.querySelector("#passo")
 
+function spamer(index,resu){
+
+    let span = document.createElement('span')
+    span.innerHTML = `&nbsp ${index}`
+
+    if (resu===true){
+        span.innerHTML += " ->"
+    }
+
+    res.appendChild(span)
+}
+
 function contar(){
+
     inicio.value = inicio.value || 0
     fim.value = fim.value || 0
     passo.value = passo.value || 1
+
     let ini = Number(inicio.value)
     let fini = Number(fim.value)
     let passi = Number(passo.value)
 
     res.innerHTML =""
-
-    function spamer(index,resu){
-        let span = document.createElement('span')
-        span.innerHTML = `&nbsp ${index}`
-        if (resu===true){
-            span.innerHTML += " ->"
-        }
-        res.appendChild(span)
-    }
 
     if(ini===fini){
         spamer(ini)
@@ -32,6 +37,7 @@ function contar(){
             spamer(index,resu)
         }
     }
+    
     else if (fini<ini){
         for (let index = ini; index >= fini; index -= passi) {
             let resu = index-passi>=fini
