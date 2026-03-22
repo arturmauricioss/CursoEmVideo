6efb12fda396fe4225762587e046a6e7a42e7236
Artur Mauricio <arturmauricioss@outlook.com>
2026-03-21 23:23:02 -0300

    adicionando exercicio 16 100% solo

---
+++ aula16/ex016/script.js
@@ -0,0 +1,59 @@
+let res = document.getElementById("res")
+let select = document.querySelector("#select")
+let input = document.querySelector("input#number")
+var lista = []
+
+
+function adicionar(){
+    res.innerHTML=""
+    let n = Number(input.value)
+    if (n==lista.length<0){
+        console.log('nenhum item adicionado')
+    } else {
+    let validar = false
+    for(let pos = 0; pos<lista.length;pos++){
+        if (n != lista[pos]){
+            console.log(`${n} não é igual a ${lista[pos]}`)
+        } else {
+            console.log(`${n} é igual a ${lista[pos]}`)
+            validar = true
+            break
+        }
+    }
+    console.log(validar)
+    if (validar===true){
+        console.log(`Numero já adicionado a lista`)
+
+    } else{
+        lista.push(input.value)
+        console.log(lista)
+        
+        select.innerHTML += `<option>${n}</option>`
+    }
+
+    input.value = ""
+    }
+}
+
+function calcular(){
+    
+    res.innerHTML=`<p>Ao todo temos ${lista.length} números na lista</p>`
+    let resultado = Number(0)
+    for (var i = 0; i<lista.length; i++){
+        
+        valor = lista[i]
+        resultado += Number(valor)
+    }
+   
+    res.innerHTML+=`<p>A soma é ${resultado}</p>`
+
+    if (lista.length<1){
+        console.log('nenhum item adicionado')
+        res.innerHTML+=`<p>A média de nada é nada</p>`
+    } else {
+        let media = resultado/lista.length
+
+        media = media.toFixed(2)
+        res.innerHTML+=`<p>A média é ${media}</p>`
+    }
+}
